578947b706312880de3c08f15f9dd58706c82de1
Akshat Maheshwari <0.akshat.maheshwari@gmail.com>
2023-04-24 13:51:46 +0530

    url change

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -10,7 +10,7 @@ export function useAuth(){
 export function AuthProvider({children}) {
     const [currentUser, setCurrentUser]= useState();
     const [loading, setLoading] = useState(true);
-    const baseURL="https://backend-jxv0.onrender.com/";
+    const baseURL="https://backend-jxv0.onrender.com";
 
     const value={
         currentUser,
